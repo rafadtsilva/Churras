1719a8edb44e3c8d06a08323dc66eb090b868966
Rafael Trindade <rafaeltrindade@outlook.com>
2021-06-16 17:38:59 -0300

    fase 2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let inputDuracao = document.getElementById("duracao");
 let resultado = document.getElementById("resultado");
 
 function calcular() {
+
     let adultos = inputAdultos.value;
     let crianças = inputCriancas.value;
     let duracao = inputDuracao.value;
@@ -22,5 +23,9 @@ function calcular() {
     let qtdTotalCarne = carne * adultos + (carne/2 * crianças);
     let qtdTotalCerveja = cerveja * adultos;
     let qtdTotalBebida = carne * adultos + (carne/2 * crianças);
-    console.log(qtdTotalCarne);
+    // console.log(qtdTotalCarne);
+
+    resultado.innerHTML = '<p>${qtdTotalCarne}g de Carnes</p>'
+    resultado.innerHTML += '<p>${qtdTotalCerveja}ml de Cerveja</p>'
+    resultado.innerHTML += '<p>$qdtTotalBebida}ml de Bebidas</p>'
 }
